f57333e53fd402324caad685de26637dd26e5948
Julian A Avar C <julian-a-avar-c@proton.me>
2025-06-13 16:59:04 -0400

    Add: Finish desktop menu

--- src/components/layout/HeaderNav.tsx
+++ src/components/layout/HeaderNav.tsx
@@ -4,7 +4,14 @@ import * as React from "react";
 
 import { config as mainConfig } from "@/config/main";
 
-import { productos } from "@/config/productos";
+import {
+  categorias,
+  presetarCategoria,
+  productos,
+  productosDeCategoria,
+  type Categoria,
+  type Producto,
+} from "@/config/productos";
 
 import { cn } from "@/lib/utils";
 import {
@@ -52,116 +59,66 @@ export function HeaderNav({ class: className }: HeaderNavProps) {
           </NavigationMenuLink>
         </NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <NavigationMenuTrigger
-            className={cn(
-              navigationMenuTriggerStyle(),
-              "text-lg text-shadow font-semibold bg-background/0 rounded-none"
-            )}
-          >
-            <a href="/indumentaria" style={{ textDecorationLine: "none" }}>
-              Indumentaria
-            </a>
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul
+        {[
+          {
+            key: "indumentaria",
+            href: "/indumentaria",
+            titulo: "Indumentaria",
+            categorias: ["top", "remera", "buzo", "polera"] as Categoria[],
+          },
+          {
+            key: "ceramicas",
+            href: "/ceramicas",
+            titulo: "Ceramicas",
+            categorias: ["pocillo", "taza", "plato", "portavasos"] as Categoria[],
+          },
+          {
+            key: "otros",
+            href: "/otros",
+            titulo: "Otros",
+            categorias: ["otro"] as Categoria[],
+          },
+        ].map((item) => (
+          <NavigationMenuItem key={item.key}>
+            <NavigationMenuTrigger
               className={cn(
-                "flex flex-col",
-                "w-[400px] gap-3 p-4 md:w-[500px] lg:w-[600px]"
+                navigationMenuTriggerStyle(),
+                "text-lg text-shadow font-semibold bg-background/0 rounded-none"
               )}
             >
-              <div className="md:row-[1/11]">
-                {productos.indumentaria.map(
-                  (producto) => (
-                    // TODO: href
-                    <ListItem
-                      key={`top-nav-productos-${producto.id}`}
-                      image={producto.imagenes[0]}
-                      title={producto.nombre}
-                      href={`/?producto=${producto.id}`}
-                    >
-                      {producto.categorias.join(", ")}
-                    </ListItem>
-                  )
-                )}
-              </div>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger
-            className={cn(
-              navigationMenuTriggerStyle(),
-              "text-lg text-shadow font-semibold bg-background/0 rounded-none"
-            )}
-          >
-            <a href="/ceramicas" style={{ textDecorationLine: "none" }}>
-              Ceramicas
-            </a>
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul
-              className={cn(
-                "flex flex-col",
-                "w-[400px] gap-3 p-4 md:w-[500px] lg:w-[600px]"
-              )}
-            >
-              <div className="md:row-[1/11]">
-                {productos.ceramica.map(
-                  (producto) => (
-                    // TODO: href
-                    <ListItem
-                      key={`top-nav-productos-${producto.id}`}
-                      image={producto.imagenes[0]}
-                      title={producto.nombre}
-                      href={`/?producto=${producto.id}`}
-                    >
-                      {producto.categorias.join(", ")}
-                    </ListItem>
-                  )
-                )}
-              </div>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger
-            className={cn(
-              navigationMenuTriggerStyle(),
-              "text-lg text-shadow font-semibold bg-background/0 rounded-none"
-            )}
-          >
-            <a href="/otros" style={{ textDecorationLine: "none" }}>
-              Otros
-            </a>
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul
-              className={cn(
-                "flex flex-col",
-                "w-[400px] gap-3 p-4 md:w-[500px] lg:w-[600px]"
-              )}
-            >
-              <div className="md:row-[1/11]">
-                {productos.otros.map(
-                  (producto) => (
-                    // TODO: href
-                    <ListItem
-                      key={`top-nav-productos-${producto.id}`}
-                      image={producto.imagenes[0]}
-                      title={producto.nombre}
-                      href={`/?producto=${producto.id}`}
-                    >
-                      {producto.categorias.join(", ")}
-                    </ListItem>
-                  )
-                )}
-              </div>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+              <a href={item.href} style={{ textDecorationLine: "none" }}>
+                {item.titulo}
+              </a>
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              {
+                (item.categorias ?? []).map((categoria) => ({categoria, productos: productosDeCategoria(categoria)}))
+                .filter(({categoria, productos}) => productos.length > 0)
+                .map(({categoria, productos}) => {
+                  return (
+                    <div key="flex flex-col gap-4">
+                      <div>{presetarCategoria(categoria as Categoria)}</div>
+                      <ul
+                        className={cn(
+                          "flex flex-wrap",
+                          "w-[400px] gap-3 p-4 md:w-[500px] lg:w-[600px]"
+                        )}
+                      >
+                        {productos.map((producto) => (
+                          // TODO: href
+                          <CircleListItem
+                            key={`top-nav-productos-${producto.id}`}
+                            image={producto.imagenes[0]}
+                            className="rounded-full w-fit"
+                          />
+                        ))}
+                      </ul>
+                    </div>
+                  );
+                })}
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        ))}
 
         <NavigationMenuItem>
           <NavigationMenuLink
@@ -193,12 +150,12 @@ export function HeaderNav({ class: className }: HeaderNavProps) {
   );
 }
 
-const ListItem = React.forwardRef<
+const TextListItem = React.forwardRef<
   React.ComponentRef<"a">,
   React.ComponentPropsWithoutRef<"a"> & {
     listClassName?: string;
     titleClass?: string;
-    image: string,
+    image: string;
   }
 >(
   (
@@ -222,13 +179,23 @@ const ListItem = React.forwardRef<
             style={{ textDecorationLine: "none" }}
             {...props}
           >
-            <img src={image} className="aspect-square rounded-full row-[1/3] col-[1/2] w-16" />
-            <div className={cn("text-sm font-medium leading-none", "row-[1/2]", titleClass)}>
+            <img
+              src={image}
+              className="aspect-square rounded-full row-[1/3] col-[1/2] w-16"
+            />
+            <div
+              className={cn(
+                "text-sm font-medium leading-none",
+                "row-[1/2]",
+                titleClass
+              )}
+            >
               {title}
             </div>
             <p
               className={cn(
-                "line-clamp-2 text-sm leading-snug text-muted-foreground", "row-[2/3]"
+                "line-clamp-2 text-sm leading-snug text-muted-foreground",
+                "row-[2/3]"
               )}
             >
               {children}
@@ -239,4 +206,44 @@ const ListItem = React.forwardRef<
     );
   }
 );
-ListItem.displayName = "ListItem";
+TextListItem.displayName = "TextListItem";
+
+const CircleListItem = React.forwardRef<
+  React.ComponentRef<"a">,
+  React.ComponentPropsWithoutRef<"a"> & {
+    listClassName?: string;
+    titleClass?: string;
+    image: string;
+  }
+>(
+  (
+    { className, listClassName, image, title, children, titleClass, ...props },
+    ref
+  ) => {
+    return (
+      <li className={cn(listClassName)}>
+        <NavigationMenuLink asChild>
+          <a
+            ref={ref}
+            className={cn(
+              "leading-none",
+              "gap-y-1 p-3",
+              "gap-x-4",
+              "select-none rounded-full hover:rounded-full no-underline outline-none transition-colors",
+              "hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+              className
+            )}
+            style={{ textDecorationLine: "none" }}
+            {...props}
+          >
+            <img
+              src={image}
+              className="aspect-square rounded-full row-[1/3] col-[1/2] w-16"
+            />
+          </a>
+        </NavigationMenuLink>
+      </li>
+    );
+  }
+);
+CircleListItem.displayName = "CircleListItem";
--- src/config/productos.ts
+++ src/config/productos.ts
@@ -1,14 +1,36 @@
-type Categoria =
-  | "top"
-  | "remera"
-  | "buzo"
-  | "polera"
-  | "pocillo"
-  | "taza"
-  | "plato"
-  | "portavasos"
-  | "oferta"
-  | "otro";
+const categorias = [
+  "top",
+  "remera",
+  "buzo",
+  "polera",
+  "pocillo",
+  "taza",
+  "plato",
+  "portavasos",
+  "oferta",
+  "otro",
+] as const;
+type Categoria = (typeof categorias)[number];
+type ProductosIndumentarios = Categoria &
+  ("top" | "remera" | "buzo" | "polera");
+type ProductosCeramicos = Categoria &
+  ("pocillo" | "taza" | "plato" | "portavasos");
+function presetarCategoria(categoria: Categoria) {
+  switch (categoria) {
+    case "top": return "Top"
+    case "remera": return "Remeras"
+    case "buzo": return "Buzos"
+    case "polera": return "Poleras"
+    case "pocillo": return "Pocillos"
+    case "taza": return "Tazas"
+    case "plato": return "Platos"
+    case "portavasos": return "Portavasos"
+    case "oferta": return "Ofertas"
+    case "otro": return "Otros"
+  }
+}
+export { categorias, presetarCategoria };
+export { type Categoria}
 
 function talleStandard(categoria: Categoria) {
   return (
@@ -53,234 +75,240 @@ type Producto = {
   imagenes: string[];
 };
 
-const productos: {
-  indumentaria: Producto[];
-  ceramica: Producto[];
-  otros: Producto[];
-} = {
-  indumentaria: [
-    {
-      id: "top-escarabajo",
-      nombre: "Escarabajo top",
-      categorias: ["top"],
-      talles: talleStandard("top"),
-      precio: precioStandard("top"),
-      imagenes: [
-        "/imagenes/productos/top-escarabajo/insecto-rojo-0.jpg",
-        "/imagenes/productos/top-escarabajo/insecto-rojo-1.jpg",
-      ],
-    },
-    {
-      id: "top-libelula",
-      nombre: "Libelula",
-      categorias: ["top"],
-      talles: talleStandard("top"),
-      precio: precioStandard("top"),
-      imagenes: [
-        "/imagenes/productos/top-libelula/libelula-0.jpg",
-        "/imagenes/productos/top-libelula/libelula-1.jpg",
-      ],
-    },
-    {
-      id: "top-mariposa",
-      nombre: "Mariposa top",
-      categorias: ["top"],
-      talles: talleStandard("top"),
-      precio: precioStandard("top"),
-      imagenes: [
-        "/imagenes/productos/top-mariposa/mariposa-0.jpg",
-        "/imagenes/productos/top-mariposa/mariposa-1.jpg",
-      ],
-    },
-    {
-      id: "top-medusa",
-      nombre: "Medusa top",
-      categorias: ["top"],
-      talles: talleStandard("top"),
-      precio: precioStandard("top"),
-      imagenes: [
-        "/imagenes/productos/top-medusa/medusa-top-0.jpg",
-        "/imagenes/productos/top-medusa/medusa-top-1.jpg",
-      ],
-    },
-    {
-      id: "top-pulpo",
-      nombre: "Pulpo",
-      categorias: ["top"],
-      talles: talleStandard("top"),
-      precio: precioStandard("top"),
-      imagenes: ["/imagenes/productos/top-pulpo/pulpo-0.jpg"],
-    },
-    {
-      id: "remera-insectos",
-      nombre: "Insectos",
-      categorias: ["remera"],
-      talles: talleStandard("remera"),
-      precio: precioStandard("remera"),
-      imagenes: [
-        "/imagenes/productos/remera-insectos/insectos-0.jpg",
-        "/imagenes/productos/remera-insectos/insectos-1.jpg",
-        "/imagenes/productos/remera-insectos/insectos-2.jpg",
-        "/imagenes/productos/remera-insectos/insectos-3.jpg",
-      ],
-    },
-    {
-      id: "remera-pez",
-      nombre: "Pez",
-      categorias: ["remera"],
-      talles: talleStandard("remera"),
-      precio: precioStandard("remera"),
-      imagenes: [
-        "/imagenes/productos/remera-pez/pez-0.jpg",
-        "/imagenes/productos/remera-pez/pez-1.jpg",
-      ],
-    },
-  ],
+const productos: Producto[] = [
+  // indumentaria: [
+  {
+    id: "top-escarabajo",
+    nombre: "Escarabajo top",
+    categorias: ["top"],
+    talles: talleStandard("top"),
+    precio: precioStandard("top"),
+    imagenes: [
+      "/imagenes/productos/top-escarabajo/insecto-rojo-0.jpg",
+      "/imagenes/productos/top-escarabajo/insecto-rojo-1.jpg",
+    ],
+  },
+  {
+    id: "top-libelula",
+    nombre: "Libelula",
+    categorias: ["top"],
+    talles: talleStandard("top"),
+    precio: precioStandard("top"),
+    imagenes: [
+      "/imagenes/productos/top-libelula/libelula-0.jpg",
+      "/imagenes/productos/top-libelula/libelula-1.jpg",
+    ],
+  },
+  {
+    id: "top-mariposa",
+    nombre: "Mariposa top",
+    categorias: ["top"],
+    talles: talleStandard("top"),
+    precio: precioStandard("top"),
+    imagenes: [
+      "/imagenes/productos/top-mariposa/mariposa-0.jpg",
+      "/imagenes/productos/top-mariposa/mariposa-1.jpg",
+    ],
+  },
+  {
+    id: "top-medusa",
+    nombre: "Medusa top",
+    categorias: ["top"],
+    talles: talleStandard("top"),
+    precio: precioStandard("top"),
+    imagenes: [
+      "/imagenes/productos/top-medusa/medusa-top-0.jpg",
+      "/imagenes/productos/top-medusa/medusa-top-1.jpg",
+    ],
+  },
+  {
+    id: "top-pulpo",
+    nombre: "Pulpo",
+    categorias: ["top"],
+    talles: talleStandard("top"),
+    precio: precioStandard("top"),
+    imagenes: ["/imagenes/productos/top-pulpo/pulpo-0.jpg"],
+  },
+  {
+    id: "remera-insectos",
+    nombre: "Insectos",
+    categorias: ["remera"],
+    talles: talleStandard("remera"),
+    precio: precioStandard("remera"),
+    imagenes: [
+      "/imagenes/productos/remera-insectos/insectos-0.jpg",
+      "/imagenes/productos/remera-insectos/insectos-1.jpg",
+      "/imagenes/productos/remera-insectos/insectos-2.jpg",
+      "/imagenes/productos/remera-insectos/insectos-3.jpg",
+    ],
+  },
+  {
+    id: "remera-pez",
+    nombre: "Pez",
+    categorias: ["remera"],
+    talles: talleStandard("remera"),
+    precio: precioStandard("remera"),
+    imagenes: [
+      "/imagenes/productos/remera-pez/pez-0.jpg",
+      "/imagenes/productos/remera-pez/pez-1.jpg",
+    ],
+  },
+  // ],
 
   // --------------------------------------------------------------------------
 
-  ceramica: [
-    {
-      id: "taza-fairy",
-      nombre: "Taza fairy core",
-      categorias: ["taza"],
-      talles: talleStandard("taza"),
-      precio: precioStandard("taza"),
-      imagenes: [
-        "/imagenes/productos/taza-fairy-core/taza-fairy-core-0.jpg",
-        "/imagenes/productos/taza-fairy-core/taza-fairy-core-1.jpg",
-        "/imagenes/productos/taza-fairy-core/taza-fairy-core-2.jpg",
-      ],
-    },
-    {
-      id: "plato-seimack",
-      nombre: "Plato seimack",
-      categorias: ["plato"],
-      talles: talleStandard("plato"),
-      precio: precioStandard("plato"),
-      imagenes: ["/imagenes/productos/plato-seimack/plato-seimack-0.jpg"],
-    },
-    {
-      id: "pocillo-catlove",
-      nombre: "Pocillo catlove",
-      categorias: ["pocillo"],
-      talles: talleStandard("pocillo"),
-      precio: precioStandard("pocillo"),
-      imagenes: [
-        "/imagenes/productos/pocillo-catlove/pocillo-catlove-0.jpg",
-        "/imagenes/productos/pocillo-catlove/pocillo-catlove-1.jpg",
-        "/imagenes/productos/pocillo-catlove/pocillo-catlove-2.jpg",
-      ],
-    },
-    {
-      id: "pocillo-libelula",
-      nombre: "Pocillo libelula",
-      categorias: ["pocillo"],
-      talles: talleStandard("pocillo"),
-      precio: precioStandard("pocillo"),
-      imagenes: [
-        "/imagenes/productos/pocillo-libelula/pocillo-libelula-0.jpg",
-        "/imagenes/productos/pocillo-libelula/pocillo-libelula-1.jpg",
-      ],
-    },
-    {
-      id: "pocillo-arque",
-      nombre: "Pocillo arque",
-      categorias: ["pocillo"],
-      talles: talleStandard("pocillo"),
-      precio: precioStandard("pocillo"),
-      imagenes: ["/imagenes/productos/pocillo-arque/pocillo-arque-0.jpg"],
-    },
-  ],
+  // ceramicas: [
+  {
+    id: "taza-fairy",
+    nombre: "Taza fairy core",
+    categorias: ["taza"],
+    talles: talleStandard("taza"),
+    precio: precioStandard("taza"),
+    imagenes: [
+      "/imagenes/productos/taza-fairy-core/taza-fairy-core-0.jpg",
+      "/imagenes/productos/taza-fairy-core/taza-fairy-core-1.jpg",
+      "/imagenes/productos/taza-fairy-core/taza-fairy-core-2.jpg",
+    ],
+  },
+  {
+    id: "plato-seimack",
+    nombre: "Plato seimack",
+    categorias: ["plato"],
+    talles: talleStandard("plato"),
+    precio: precioStandard("plato"),
+    imagenes: ["/imagenes/productos/plato-seimack/plato-seimack-0.jpg"],
+  },
+  {
+    id: "pocillo-catlove",
+    nombre: "Pocillo catlove",
+    categorias: ["pocillo"],
+    talles: talleStandard("pocillo"),
+    precio: precioStandard("pocillo"),
+    imagenes: [
+      "/imagenes/productos/pocillo-catlove/pocillo-catlove-0.jpg",
+      "/imagenes/productos/pocillo-catlove/pocillo-catlove-1.jpg",
+      "/imagenes/productos/pocillo-catlove/pocillo-catlove-2.jpg",
+    ],
+  },
+  {
+    id: "pocillo-libelula",
+    nombre: "Pocillo libelula",
+    categorias: ["pocillo"],
+    talles: talleStandard("pocillo"),
+    precio: precioStandard("pocillo"),
+    imagenes: [
+      "/imagenes/productos/pocillo-libelula/pocillo-libelula-0.jpg",
+      "/imagenes/productos/pocillo-libelula/pocillo-libelula-1.jpg",
+    ],
+  },
+  {
+    id: "pocillo-arque",
+    nombre: "Pocillo arque",
+    categorias: ["pocillo"],
+    talles: talleStandard("pocillo"),
+    precio: precioStandard("pocillo"),
+    imagenes: ["/imagenes/productos/pocillo-arque/pocillo-arque-0.jpg"],
+  },
+  // ],
 
   // --------------------------------------------------------------------------
 
-  otros: [
-    {
-      id: "taza-minion",
-      nombre: "Taza minion",
-      categorias: ["pocillo", "otro", "oferta"],
-      precio: "$7mil",
-      imagenes: [
-        "/imagenes/productos/taza-minion/taza-minion-0.jpg",
-        "/imagenes/productos/taza-minion/taza-minion-1.jpg",
-        "/imagenes/productos/taza-minion/taza-minion-2.jpg",
-      ],
-    },
-    {
-      id: "taza-mariposa",
-      nombre: "Taza mariposa",
-      categorias: ["pocillo", "otro", "oferta"],
-      precio: "$7mil",
-      imagenes: [
-        "/imagenes/productos/taza-mariposa/taza-mariposa-0.jpg",
-        "/imagenes/productos/taza-mariposa/taza-mariposa-1.jpg",
-      ],
-    },
-    {
-      id: "remera-eyes",
-      nombre: "Remera eyes",
-      categorias: ["remera", "otro", "oferta"],
-      precio: "$10mil",
-      imagenes: [
-        "/imagenes/productos/remera-eyes/remera-eyes-0.jpg",
-        "/imagenes/productos/remera-eyes/remera-eyes-1.jpg",
-        "/imagenes/productos/remera-eyes/remera-eyes-2.jpg",
-      ],
-    },
-    {
-      id: "plato-trepez",
-      nombre: "Plato trepez",
-      categorias: ["plato", "otro", "oferta"],
-      precio: "$7mil",
-      imagenes: ["/imagenes/productos/plato-trepez/plato-trepez-0.jpg"],
-    },
-    {
-      id: "portavasos",
-      nombre: "Portavasos",
-      categorias: ["portavasos"],
-      precio: precioStandard("portavasos"),
-      imagenes: [
-        "/imagenes/productos/portavasos/portavasos-0.jpg",
-        "/imagenes/productos/portavasos/portavasos-1.jpg",
-      ],
-    },
-    {
-      id: "portavasos-set",
-      nombre: "Portavasos set",
-      categorias: ["otro"],
-      precio: "$30mil",
-      imagenes: [
-        "/imagenes/productos/portavasos-set/portavasos-set-0.jpg",
-        "/imagenes/productos/portavasos-set/portavasos-set-1.jpg",
-        "/imagenes/productos/portavasos-set/portavasos-set-2.jpg",
-        "/imagenes/productos/portavasos-set/portavasos-set-3.jpg",
-      ],
-    },
-    {
-      id: "taza-maceta",
-      nombre: "Maceta Pants",
-      categorias: ["taza", "oferta", "otro"],
-      talles: talleStandard("taza"),
-      precio: "$10mil",
-      imagenes: [
-        "/imagenes/productos/taza-maceta-pants/maceta-pants-0.jpg",
-        "/imagenes/productos/taza-maceta-pants/maceta-pants-1.jpg",
-        "/imagenes/productos/taza-maceta-pants/maceta-pants-2.jpg",
-      ],
-    },
-    {
-      id: "taza-frutilla",
-      nombre: "Taza frutilla",
-      categorias: ["taza", "oferta", "otro"],
-      precio: "$25mil",
-      imagenes: [
-        "/imagenes/productos/taza-frutilla/taza-frutilla-0.jpg",
-        "/imagenes/productos/taza-frutilla/taza-frutilla-1.jpg",
-        "/imagenes/productos/taza-frutilla/taza-frutilla-2.jpg",
-      ],
-    },
-  ],
-};
+  // otros: [
+  {
+    id: "taza-minion",
+    nombre: "Taza minion",
+    categorias: ["pocillo", "otro", "oferta"],
+    precio: "$7mil",
+    imagenes: [
+      "/imagenes/productos/taza-minion/taza-minion-0.jpg",
+      "/imagenes/productos/taza-minion/taza-minion-1.jpg",
+      "/imagenes/productos/taza-minion/taza-minion-2.jpg",
+    ],
+  },
+  {
+    id: "taza-mariposa",
+    nombre: "Taza mariposa",
+    categorias: ["pocillo", "otro", "oferta"],
+    precio: "$7mil",
+    imagenes: [
+      "/imagenes/productos/taza-mariposa/taza-mariposa-0.jpg",
+      "/imagenes/productos/taza-mariposa/taza-mariposa-1.jpg",
+    ],
+  },
+  {
+    id: "remera-eyes",
+    nombre: "Remera eyes",
+    categorias: ["remera", "otro", "oferta"],
+    precio: "$10mil",
+    imagenes: [
+      "/imagenes/productos/remera-eyes/remera-eyes-0.jpg",
+      "/imagenes/productos/remera-eyes/remera-eyes-1.jpg",
+      "/imagenes/productos/remera-eyes/remera-eyes-2.jpg",
+    ],
+  },
+  {
+    id: "plato-trepez",
+    nombre: "Plato trepez",
+    categorias: ["plato", "otro", "oferta"],
+    precio: "$7mil",
+    imagenes: ["/imagenes/productos/plato-trepez/plato-trepez-0.jpg"],
+  },
+  {
+    id: "portavasos",
+    nombre: "Portavasos",
+    categorias: ["portavasos"],
+    precio: precioStandard("portavasos"),
+    imagenes: [
+      "/imagenes/productos/portavasos/portavasos-0.jpg",
+      "/imagenes/productos/portavasos/portavasos-1.jpg",
+    ],
+  },
+  {
+    id: "portavasos-set",
+    nombre: "Portavasos set",
+    categorias: ["otro"],
+    precio: "$30mil",
+    imagenes: [
+      "/imagenes/productos/portavasos-set/portavasos-set-0.jpg",
+      "/imagenes/productos/portavasos-set/portavasos-set-1.jpg",
+      "/imagenes/productos/portavasos-set/portavasos-set-2.jpg",
+      "/imagenes/productos/portavasos-set/portavasos-set-3.jpg",
+    ],
+  },
+  {
+    id: "taza-maceta",
+    nombre: "Maceta Pants",
+    categorias: ["taza", "oferta", "otro"],
+    talles: talleStandard("taza"),
+    precio: "$10mil",
+    imagenes: [
+      "/imagenes/productos/taza-maceta-pants/maceta-pants-0.jpg",
+      "/imagenes/productos/taza-maceta-pants/maceta-pants-1.jpg",
+      "/imagenes/productos/taza-maceta-pants/maceta-pants-2.jpg",
+    ],
+  },
+  {
+    id: "taza-frutilla",
+    nombre: "Taza frutilla",
+    categorias: ["taza", "oferta", "otro"],
+    precio: "$25mil",
+    imagenes: [
+      "/imagenes/productos/taza-frutilla/taza-frutilla-0.jpg",
+      "/imagenes/productos/taza-frutilla/taza-frutilla-1.jpg",
+      "/imagenes/productos/taza-frutilla/taza-frutilla-2.jpg",
+    ],
+  },
+];
 
-export { productos };
+function productosDeCategoria(...categorias: Categoria[]): Producto[] {
+  // return productos.filter((producto) =>
+  //   producto.categorias.some(categorias.includes)
+  // );
+  return productos.filter(producto => 
+    producto.categorias.some(categoria => categorias.includes(categoria))
+    // categorias.some(categoria => categorias.includes(categoria))
+  )
+}
+
+export { productos, productosDeCategoria };
+export {type Producto}
